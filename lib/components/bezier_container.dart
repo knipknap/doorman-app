8f0d0004bbebea9845099dd6e53cad40d29ae1bd
Samuel Abels <knipknap@gmail.com>
2021-12-09 22:06:15 +0100

    smooth some edges on the bezier design; simplify implementation

--- lib/components/bezier_container.dart
+++ lib/components/bezier_container.dart
@@ -1,7 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-
 import 'clip_painter.dart';
 
 class BezierContainer extends StatelessWidget {
@@ -9,24 +6,21 @@ class BezierContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Transform.rotate(
-      angle: -pi / 3.5, 
-      child: ClipPath(
-        clipper: ClipPainter(),
-        child: Container(
-          height: MediaQuery.of(context).size.height *.5,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: const [
-                  Color(0xfffbb448),
-                  Color(0xffe46b10),
-                ]
-              )
-            ),
-        ),
+    return ClipPath(
+      clipper: ClipPainter(),
+      child: Container(
+        height: MediaQuery.of(context).size.height *.55,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: const [
+                Color(0xfffbb448),
+                Color(0xffe46b10),
+              ]
+            )
+          ),
       ),
     );
   }
--- lib/components/clip_painter.dart
+++ lib/components/clip_painter.dart
@@ -7,26 +7,13 @@ class ClipPainter extends CustomClipper<Path>{
     var width = size.width;
     var path = Path();
 
-    path.lineTo(0, size.height);
-    path.lineTo(size.width, height);
-    path.lineTo(size.width, 0);
-  
-     /// [Top Left corner]
-    var secondControlPoint =  Offset(0 ,0);
-    var secondEndPoint = Offset(width * .18, height *.3);
-    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);
-
-     /// [Left Middle]
-    var fifthControlPoint =  Offset(width * .3, height * .5);
-    var fiftEndPoint = Offset(width * .23, height *.6);
-    path.quadraticBezierTo(fifthControlPoint.dx, fifthControlPoint.dy, fiftEndPoint.dx, fiftEndPoint.dy);
-
-     /// [Bottom Left corner]
-    var thirdControlPoint =  Offset(0, height);
-    var thirdEndPoint = Offset(width, height*1.8);
-    path.quadraticBezierTo(thirdControlPoint.dx, thirdControlPoint.dy, thirdEndPoint.dx, thirdEndPoint.dy);
-
-    path.lineTo(0, size.height  );
+    path.lineTo(-1, size.height+1);
+    path.lineTo(width, height);
+    path.lineTo(width, 0);
+    path.quadraticBezierTo(-width*.2, 0, width*.15, height*.3+10);
+    path.quadraticBezierTo(width*.35, height*.45, width*.3, height*.6);
+    path.quadraticBezierTo(width*.2, height, width, height);
+    path.lineTo(-1, size.height+1);
     path.close();
 
     return path;
@@ -34,8 +21,6 @@ class ClipPainter extends CustomClipper<Path>{
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
-    // ignore: todo
-    // TODO: implement shouldReclip
-    return true;
+    return false;
   }
 }
